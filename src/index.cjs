19f3ac5db0cf97a91a25c3b60598c33d5b4777d1
Pcrab <pcrab@pcrab.xyz>
2023-04-13 12:41:07 +0800

    style: reformat and add type annotation

--- src/index.cjs
+++ src/index.cjs
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 const renderer = require("./renderer.cjs");
 
--- src/renderer.cjs
+++ src/renderer.cjs
@@ -1,36 +1,43 @@
-"use strict"
-const rExcerpt = /<!-- ?more ?-->/i;
+"use strict";
 const djot = require("@djot/djot");
 const katex = require("katex");
 
-function renderer(data, options) {
-
+function renderer(data, _) {
+    // render excerpt sign to raw html
+    const rExcerpt = /<!-- ?more ?-->/i;
     data.text = data.text.replace(rExcerpt, "``` =html\n<!-- more -->\n```");
 
-    const parseOptions = {}
+    /** @type {import("@djot/djot/types/parse").ParseOptions} */
+    const parseOptions = {};
     this.execFilterSync("djot:parse", parseOptions);
 
+    /** @type {import("@djot/djot/types/html").HTMLRenderOptions["overrides"]} */
     const renderOverrides = {
         inline_math: (node) => {
             try {
-                return katex.renderToString(node.text, {output: "mathml"});
-            } catch(e) {
+                return katex.renderToString(node.text, { output: "mathml" });
+            } catch (e) {
                 console.error(e);
-                return `<span class="math inline">${node.text}</span>`
+                return `<span class="math inline">${node.text}</span>`;
             }
         },
         display_math: (node) => {
             try {
-                return katex.renderToString(node.text, {displayMode: true, output: "mathml"});
-            } catch(e) {
+                return katex.renderToString(node.text, {
+                    displayMode: true,
+                    output: "mathml",
+                });
+            } catch (e) {
                 console.error(e);
-                return `<span class="math inline">${node.text}</span>`
+                return `<span class="math inline">${node.text}</span>`;
             }
         },
-    }
+    };
     this.execFilterSync("djot:renderer", renderOverrides);
 
-    return djot.renderHTML(djot.parse(data.text, parseOptions), {overrides: renderOverrides});
+    return djot.renderHTML(djot.parse(data.text, parseOptions), {
+        overrides: renderOverrides,
+    });
 }
 
 module.exports = renderer;
